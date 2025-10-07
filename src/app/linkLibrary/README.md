30629aa46cd22f64dca1568d842413d2eba7167b
BananaJeans <18456310+BananaJeanss@users.noreply.github.com>
2025-10-07 12:52:47 +0300

    feat: improved linklibrary json format, added table of contents, added contributing guide, added smooth scroll to html

---
+++ src/app/linkLibrary/README.md
@@ -0,0 +1,17 @@
+# Library of Links
+
+For contributors:
+If you wish to add a new link to the library of links, doing so should be simple.
+
+The added link should be UT/DR related of course, and should not have any typos.
+
+The following format is used for any entries:
+
+```json
+{
+    "category": "Deltarune",
+    "title": "Deltarune Wiki",
+    "description": "The official Deltarune Wiki."
+    "link": "https://deltarune.wiki/"
+}
+```
--- src/app/linkLibrary/page.tsx
+++ src/app/linkLibrary/page.tsx
@@ -13,39 +13,57 @@ interface Link {
 }
 
 export default function LinkLibrary() {
-  const categorizedLinks: { [key: string]: Link[] } = {};
+  const UncategorizedLinks: { [key: string]: Link[] } = {};
 
   // categorize
-  Links.forEach((link) => {
-    if (!categorizedLinks[link.category]) {
-      categorizedLinks[link.category] = [];
-    }
-    categorizedLinks[link.category].push(link);
+  Object.entries(Links).forEach(([category, links]) => {
+    UncategorizedLinks[category] = links;
   });
+
+  // sort categories alphabetically
+  const categorizedLinks = Object.keys(UncategorizedLinks).sort();
+
   return (
-    <div>
+    <div className="p-4">
       <h1>Library of Links</h1>
-      <div className="flex flex-wrap gap-4">
-        {Object.keys(categorizedLinks).map((category) => (
-          <div key={category} className="w-full">
-            <h2>{category}</h2>
-            <ul className="list-disc ml-6">
-              {categorizedLinks[category].map((link, index) => (
-                <li key={index}>
-                  <a
-                    href={link.link}
-                    className="text-blue-500 hover:underline"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {link.title}
-                  </a>
-                  <p className="text-sm text-gray-500">{link.description}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
+      <div className="flex flex-col gap-8 sm:flex-row">
+        <div className="flex flex-wrap gap-4">
+          {categorizedLinks.map((category) => (
+            <div key={category} className="w-full">
+              <h2 id={category}>{category}</h2>
+              <ul className="list-disc ml-6">
+                {UncategorizedLinks[category].map((link, index) => (
+                  <li key={index}>
+                    <a
+                      href={link.link}
+                      className="text-blue-500 hover:underline"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {link.title}
+                    </a>
+                    <p className="text-sm text-gray-500">{link.description}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col gap-4 order-first md:order-2">
+          <h2>Table of Contents</h2>
+          <ul className="list-disc ml-6">
+            {categorizedLinks.map((category) => (
+              <li key={category}>
+                <a
+                  href={`#${category}`}
+                  className="text-blue-500 hover:underline"
+                >
+                  {category}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
